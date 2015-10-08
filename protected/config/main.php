6380be58491a0990895b3c7156c034c3f8df9263
peter <peter@polaris.local>
2015-10-09 00:10:27 +0300

    modified:   protected/config/main.php 	modified:   protected/modules/user/data/schema.mysql.sql 	modified:   protected/runtime/application.log

--- protected/config/main.php
+++ protected/config/main.php
@@ -17,6 +17,10 @@ return array(
 	'import'=>array(
 		'application.models.*',
 		'application.components.*',
+                'application.modules.user.models.*',
+                'application.modules.user.components.*',
+                'application.modules.rights.*',
+                'application.modules.rights.components.*',
 	),
 
 	'modules'=>array(
@@ -27,15 +31,79 @@ return array(
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
 		),
+                'user'=>array(
+                        'tableUsers' => 'users',
+                        'tableProfiles' => 'profiles',
+                        'tableProfileFields' => 'profiles_fields',
+                        # encrypting method (php hash function)
+                        'hash' => 'md5',
+
+                        # send activation email
+                        'sendActivationMail' => true,
+
+                        # allow access for non-activated users
+                        'loginNotActiv' => false,
+
+                        # activate user on registration (only sendActivationMail = false)
+                        'activeAfterRegister' => false,
+
+                        # automatically login from registration
+                        'autoLogin' => true,
+
+                        # registration path
+                        'registrationUrl' => array('/user/registration'),
+
+                        # recovery password path
+                        'recoveryUrl' => array('/user/recovery'),
+
+                        # login form path
+                        'loginUrl' => array('/user/login'),
+
+                        # page after login
+                        'returnUrl' => array('/user/profile'),
+
+                        # page after logout
+                        'returnLogoutUrl' => array('/user/login'),
+                ),
+
+                //Modules Rights
+               'rights'=>array(
+                       'superuserName'=>'Admin', // Name of the role with super user privileges. 
+                       'authenticatedName'=>'Authenticated',  // Name of the authenticated user role. 
+                       'userIdColumn'=>'id', // Name of the user id column in the database. 
+                       'userNameColumn'=>'username',  // Name of the user name column in the database. 
+                       'enableBizRule'=>true,  // Whether to enable authorization item business rules. 
+                       'enableBizRuleData'=>true,   // Whether to enable data for business rules. 
+                       'displayDescription'=>true,  // Whether to use item description instead of name. 
+                       'flashSuccessKey'=>'RightsSuccess', // Key to use for setting success flash messages. 
+                       'flashErrorKey'=>'RightsError', // Key to use for setting error flash messages. 
+
+                       'baseUrl'=>'/rights', // Base URL for Rights. Change if module is nested. 
+                       'layout'=>'rights.views.layouts.main',  // Layout to use for displaying Rights. 
+                       'appLayout'=>'application.views.layouts.main', // Application layout. 
+                       'cssFile'=>'rights.css', // Style sheet file to use for Rights. 
+                       'install'=>false,  // Whether to enable installer. 
+                       'debug'=>false, 
+                ),
 	),
 
 	// application components
 	'components'=>array(
 
 		'user'=>array(
-			// enable cookie-based authentication
-			'allowAutoLogin'=>true,
-		),
+                        'class'=>'RWebUser',
+                        // enable cookie-based authentication
+                        'allowAutoLogin'=>true,
+                        'loginUrl'=>array('/user/login'),
+                ),
+                'authManager'=>array(
+                        'class'=>'RDbAuthManager',
+                        'connectionID'=>'db',
+                        'itemTable'=>'authitem',
+                        'itemChildTable'=>'authitemchild',
+                        'assignmentTable'=>'authassignment',
+                        'rightsTable'=>'rights',
+                ),
 
 		// uncomment the following to enable URLs in path-format
 		/*
