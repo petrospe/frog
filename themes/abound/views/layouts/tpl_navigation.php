32529b55a5204e0729b6d9df558f85af3af4a4ca
peter <peter@polaris.local>
2015-10-11 11:39:14 +0300

    modified:   nbproject/private/private.xml 	modified:   protected/runtime/application.log 	modified:   themes/abound/views/layouts/tpl_navigation.php

--- themes/abound/views/layouts/tpl_navigation.php
+++ themes/abound/views/layouts/tpl_navigation.php
@@ -46,6 +46,7 @@
 							array('label'=>'Separated link', 'url'=>'#'),
 							array('label'=>'One more separated link', 'url'=>'#'),
                         )),
+                        array('label'=>'Rights', 'url'=>array('/rights')),
                         array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
                         array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest),
                     ),
