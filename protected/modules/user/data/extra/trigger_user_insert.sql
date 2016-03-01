0a88e2ecc70673352fbb2d315962b70e24df7dea
petrospe <petros_pe@yahoo.gr>
2016-03-01 23:06:33 +0200

    modified:   protected/modules/user/data/extra/trigger_user_insert.sql

--- protected/modules/user/data/extra/trigger_user_insert.sql
+++ protected/modules/user/data/extra/trigger_user_insert.sql
@@ -1,5 +1,4 @@
-DROP TRIGGER user_insert;
-
+DROP TRIGGER IF EXISTS user_insert;
 delimiter $$
 CREATE TRIGGER user_insert
 AFTER INSERT ON almab_customers 
@@ -8,7 +7,7 @@ FOR EACH ROW
     SET @new_dbserial = NEW.dbserial;
     SET @new_email = NEW.email;
     SET @new_descr = NEW.descr;
-   INSERT INTO `users` (`username`, `password`, `email`, `superuser`, `status`) VALUES (SUBSTRING(@new_dbserial, -9), md5(SUBSTRING(@new_dbserial, -9)), @new_email, 0, 1);
+   INSERT INTO `users` (`username`, `password`, `email`, `superuser`, `status`) VALUES (REPLACE((SUBSTRING(@new_dbserial, -9)),'-',''), md5(REPLACE((SUBSTRING(@new_dbserial, -9)),'-','')), @new_email, 0, 1);
    INSERT INTO `profiles` (`lastname`, `firstname`) VALUES (SUBSTRING_INDEX(@new_descr, ' ', 1), SUBSTRING(@new_descr, LOCATE(' ' , @new_descr)+1));
    
 END $$
