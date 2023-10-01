8aa7711763e6f0a2391ecdc91d01acd4a3932898
petrospe <petros_pe@yahoo.gr>
2023-10-01 17:37:53 +0300

    fix get_magic_quotes_gpc

--- framework/web/CHttpRequest.php
+++ framework/web/CHttpRequest.php
@@ -129,14 +129,14 @@ class CHttpRequest extends CApplicationComponent
 		// normalize request
 		// if(function_exists('get_magic_quotes_gpc') && get_magic_quotes_gpc())
 		// {
-			if(isset($_GET))
-				$_GET=$this->stripSlashes($_GET);
-			if(isset($_POST))
-				$_POST=$this->stripSlashes($_POST);
-			if(isset($_REQUEST))
-				$_REQUEST=$this->stripSlashes($_REQUEST);
-			if(isset($_COOKIE))
-				$_COOKIE=$this->stripSlashes($_COOKIE);
+		// 	if(isset($_GET))
+		// 		$_GET=$this->stripSlashes($_GET);
+		// 	if(isset($_POST))
+		// 		$_POST=$this->stripSlashes($_POST);
+		// 	if(isset($_REQUEST))
+		// 		$_REQUEST=$this->stripSlashes($_REQUEST);
+		// 	if(isset($_COOKIE))
+		// 		$_COOKIE=$this->stripSlashes($_COOKIE);
 		// }
 
 		if($this->enableCsrfValidation)
